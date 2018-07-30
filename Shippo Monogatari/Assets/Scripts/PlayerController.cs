d3c18e0f69df2956096477dd2a1535f0f3293777
Kurai <julionoronha96@gmail.com>
2018-07-30 16:21:26 -0300

    Movimentação da câmera pelo mouse
    Rotação apenas horizontal no momento

--- Shippo Monogatari/Assets/Scripts/PlayerController.cs
+++ Shippo Monogatari/Assets/Scripts/PlayerController.cs
@@ -25,15 +25,13 @@ public class PlayerController : MonoBehaviour {
 
     void MovementController()
     {
-        //Eixos do Vector3
-        //X -> Esquerda e direita
-        //Y -> Pulo
-        //Z -> Frente e trás
+        //Movimenta o personagem com as setas e WASD
+        rb.AddRelativeForce (Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"), ForceMode.Impulse);
 
-        rb.AddRelativeForce ((transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical")), ForceMode.Impulse);
-
+        //Clampa a velocidade pra não sair Sanicando por aí
         rb.velocity = new Vector3 (Mathf.Clamp(rb.velocity.x, -moveSpeed, moveSpeed),
                                    Mathf.Clamp(rb.velocity.y, -moveSpeed, moveSpeed),
                                    Mathf.Clamp(rb.velocity.z, -moveSpeed, moveSpeed));
+
     }
 }
