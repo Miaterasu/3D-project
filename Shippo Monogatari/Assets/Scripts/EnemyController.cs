d3d48954e4bd6de31b3f57fd4691328480f780f6
Kurai <julionoronha96@gmail.com>
2018-08-14 15:06:56 -0300

    Inimigo e ataques
    Movimentação do inimigo, ataque do jogador e inimigo causando dano e um leve pushback ao contato.

---
+++ Shippo Monogatari/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject weapon;
+
+    public Text uiText;
+
+    public float speed = 5f;
+    public float attackDistance = 3f;
+    public float attackCooldown = 0.5f;
+    public float knockBackPower = 5f;
+    public float immuneDuration = 0.5f;
+
+    public int hp = 50;
+    public int power = 3;
+
+    NavMeshAgent agent;
+    Rigidbody rb;
+
+    float attackCooldownTimer = 0f;
+
+    float immuneTimer = 0;
+
+    bool attacking = false;
+    bool immune = false;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        uiText.text = "Hp Enemy: " + hp;
+
+        if(attacking)
+        {
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.isStopped = false;
+        }
+
+        agent.destination = player.transform.position;
+
+        float distanceToPlayer = Mathf.Abs((transform.position - player.transform.position).magnitude);
+        if(distanceToPlayer <= attackDistance && !attacking)
+        {
+            attacking = true;
+        }
+
+        Attack();
+        Immunity();
+        Debug.Log(hp);
+	}
+
+    void Attack()
+    {
+        if (attacking)
+        {
+            weapon.transform.position = Vector3.MoveTowards(weapon.transform.position, transform.position + (transform.forward + transform.right) * 0.8f, Time.deltaTime * 10);
+            attackCooldownTimer += Time.deltaTime;
+            weapon.GetComponent<BoxCollider>().enabled = true;
+        }
+        else
+        {
+            weapon.transform.position = Vector3.MoveTowards(weapon.transform.position, transform.position + transform.right * 0.8f, Time.deltaTime * 100);
+            weapon.GetComponent<BoxCollider>().enabled = false;
+        }
+
+        if (attackCooldownTimer >= attackCooldown)
+        {
+            attacking = false;
+            attackCooldownTimer = 0f;
+        }
+    }
+
+    void Immunity()
+    {
+        if (!immune)
+        {
+            return;
+        }
+
+        immuneTimer += Time.deltaTime;
+        if (immuneTimer >= immuneDuration)
+        {
+            immuneTimer = 0f;
+            immune = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerAttack"))
+        {
+            if (!immune)
+            {
+                hp -= other.gameObject.GetComponentInParent<PlayerController>().power;
+                rb.AddForce(transform.forward * -knockBackPower, ForceMode.Acceleration);
+                immune = true;
+            }
+        }
+    }
+}
--- Shippo Monogatari/Assets/Scripts/PlayerController.cs
+++ Shippo Monogatari/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public GameObject weapon;
 
+    public Text uiText;
+
     public float moveSpeed = 5f;
     public float jumpForce = 100f;
     public float attackCooldown = 0.1f;
+    public float knockBackPower = 150f;
+    public float immuneDuration = 0.5f;
 
+    public int hp = 100;
+    public int power = 5;
     public int numberOfJumps = 1;
 
     Rigidbody rb;
 
     float attackCooldownTimer = 0;
+    float immuneTimer = 0;
 
     bool attacking = false;
+    bool immune = false;
 	
 	void Start () {
         rb = GetComponent<Rigidbody>();
 	}
 	
 	void Update () {
+        uiText.text = "Hp Player: " + hp;
+
         MovementController();
-	}
+        //Ataca
+        Attack();
+        Immunity();
+    }
     
     /*
      *  Funções sobre comandos ficam aqui 
@@ -45,17 +59,13 @@ public class PlayerController : MonoBehaviour {
         {
             rb.AddRelativeForce(0f, jumpForce, 0f, ForceMode.Impulse);
         }
-
-        //Ataca
-        Attack();
-        
+        //Debug.Log(hp);
 
     }
 
     void Attack()
     {
-        Debug.Log(attacking);
-
+        
         if (Input.GetMouseButtonDown(0) && !attacking)
         {
             attacking = true;
@@ -65,16 +75,47 @@ public class PlayerController : MonoBehaviour {
         {
             weapon.transform.position = Vector3.MoveTowards(weapon.transform.position, transform.position + (transform.forward + transform.right) * 0.8f, Time.deltaTime * 10);
             attackCooldownTimer += Time.deltaTime;
+            weapon.GetComponent<BoxCollider>().enabled = true;
         }
         else
         {
             weapon.transform.position = Vector3.MoveTowards(weapon.transform.position, transform.position + transform.right * 0.8f, Time.deltaTime * 100);
+            weapon.GetComponent<BoxCollider>().enabled = false;
         }
 
         if (attackCooldownTimer >= attackCooldown)
         {
             attacking = false;
             attackCooldownTimer = 0f;
+        }
+
+    }
+
+    void Immunity()
+    {
+        if (!immune)
+        {
+            return;
+        }
+
+        immuneTimer += Time.deltaTime;
+        if(immuneTimer >= immuneDuration)
+        {
+            immuneTimer = 0f;
+            immune = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EnemyAttack"))
+        {
+            if (!immune)
+            { 
+                hp -= other.gameObject.GetComponentInParent<EnemyController>().power;
+                rb.AddForce(transform.forward * -knockBackPower, ForceMode.Impulse);
+                immune = true;
+            }
         }
     }
 }
